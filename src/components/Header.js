2f8336ce71ebfa6519e287cc453206c446d49214
JayoonKoo <rnwkdbs12@gmail.com>
2021-06-16 22:57:19 +0900

    header 구현

---
+++ src/components/Header.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.USER_KEY = "userName";
+		this.HIDDEN_CN = "hidden";
+		this.storageName = localStorage.getItem(this.USER_KEY);
+		this.state = { userName: this.storageName }
+		
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		const loginInput = document.getElementById("login--input");
+		const userName = loginInput.value;
+		localStorage.setItem(this.USER_KEY, userName);
+		this.loginContainer.classList.add(this.HIDDEN_CN);
+		this.paintUserName(userName);
+	}
+
+	paintUserName(userName) {
+		const userNameEl = document.querySelector(".username");
+		userNameEl.classList.remove(this.HIDDEN_CN);
+		this.setState({userName});
+	}
+
+	componentDidMount() {
+		const {userName} = this.state;
+		this.loginContainer = document.querySelector(".login-container");
+		if (userName === null) {
+			this.loginContainer.classList.remove(this.HIDDEN_CN);
+		} else {
+			this.paintUserName(userName);
+		}
+	}
+
+  render() {
+		const {userName} = this.state;
+		return (
+			<header>
+				<div className="inner">
+					<div className="clock">00:00:00</div>
+					<div className="todo-container">
+						<form id="todo--form">
+							<input type="text" id="todo--input" placeholder="Today's Work"></input>
+						</form>
+					</div>
+					<div className="login-container hidden">
+						<form id="login--form" onSubmit={this.handleSubmit}>
+							<input
+								type="text"
+								id="login--input"
+								placeholder="What's your name?"
+								maxLength="15"
+								required={true}
+							></input>
+						</form>
+					</div>
+					<div className="username hidden">Hello {userName}👋</div>
+				</div>
+			</header>
+		)
+	};
+}
+
+export default Header;
